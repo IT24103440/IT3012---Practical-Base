876be5a7b59540830b775f5100d90a4d7b1d287f
IT24103440 <it24103440@my.sliit.lk>
2026-08-17 11:05:47 +0530

    Add grid size and walls to game state return

--- visual_grid_game.py
+++ visual_grid_game.py
@@ -63,11 +63,15 @@ class VisualGridHuntGame:
             ahead[0] >= self.width or
             ahead in self.walls
         )
-
+    
         return {
             "food_here": (x, y) in self.food_positions,
             "toxin_here": (x, y) in self.toxic_traps,
-            "wall_ahead": wall_ahead
+            "wall_ahead": wall_ahead,
+    
+            "grid_size": (self.width, self.height),
+            "walls": list(self.walls),
+            "all_food": list(self.food_positions)
         }
 
     def execute_action(self, action: str):
